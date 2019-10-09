23dce36e0a64df4485f2777275951ca28cc0f730
duy.nghia.tran <duy.nghia.tran@ibm.com>
2019-10-09 11:04:30 +0200

    feat/ClientThread

---
+++ src/ClientThread.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+
+public class ClientThread extends Thread{
+    private Socket socket;
+    private BufferedReader socIn;
+    private PrintStream socOut;
+
+    public ClientThread(Socket s) throws IOException {
+        this.socket = s;
+        socIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        socOut = new PrintStream(socket.getOutputStream());
+    }
+
+    public void run() {
+        while (true) {
+            try {
+                String message = socIn.readLine();
+                System.out.println("Client Thread received message: " + message);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void sendMessage(String message) {
+        socOut.println(message);
+    }
+
+    public void sendTestMessage() {
+        socOut.println("HTTP/1.0 200 OK");
+        socOut.println("Content-Type: text/html");
+        socOut.println("Server: Bot");
+        // this blank line signals the end of the headers
+        socOut.println("");
+        // Send the HTML page
+        socOut.println("<H1>Welcome to the Ultra Mini-WebServer</H2>");
+        socOut.flush();
+    }
+}
--- src/WebServer.java
+++ src/WebServer.java
@@ -39,11 +39,9 @@ public class WebServer {
             try {
                 // wait for a connection
                 Socket remote = s.accept();
+
                 // remote is now the connected socket
                 System.out.println("Connection, sending data.");
-                BufferedReader in = new BufferedReader(new InputStreamReader(
-                        remote.getInputStream()));
-                PrintWriter out = new PrintWriter(remote.getOutputStream());
 
                 // read the data sent. We basically ignore it,
                 // stop reading once a blank line is hit. This
@@ -55,14 +53,7 @@ public class WebServer {
 
                 // Send the response
                 // Send the headers
-                out.println("HTTP/1.0 200 OK");
-                out.println("Content-Type: text/html");
-                out.println("Server: Bot");
-                // this blank line signals the end of the headers
-                out.println("");
-                // Send the HTML page
-                out.println("<H1>Welcome to the Ultra Mini-WebServer</H2>");
-                out.flush();
+
                 remote.close();
             } catch (Exception e) {
                 System.out.println("Error: " + e);
@@ -80,4 +71,6 @@ public class WebServer {
         WebServer ws = new WebServer();
         ws.start();
     }
+
+    public class ClientThread
 }
